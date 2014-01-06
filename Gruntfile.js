ae36a538c899692bfb6ad7dcfdaffaa92eb68da8
Karthik Sirasanagandla <sirkarthik@gmail.com>
2014-01-06 15:33:12 +0530

    Initial commit to get started with GruntJS. Run grunt in terminal to see a warning message and exit.

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+};
