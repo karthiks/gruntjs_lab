d993116c60eb4e14ec868b604349d05b70bf2d0a
Karthik Sirasanagandla <sirkarthik@gmail.com>
2014-01-06 16:19:48 +0530

    Added JSHint to Grunt config as default task. 'grunt' or 'grunt jshint' tasks means the same

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,39 @@
+/* global module:false */
 module.exports = function(grunt) {
   // Do grunt-related things in here
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          require: true,
+          define: true,
+          requirejs: true,
+          describe: true,
+          expect: true,
+          it: true
+        }
+      }
+    }
   });
+
+  // Load JSHint task
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', 'jshint');
 };
